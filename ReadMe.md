6f9d8d0f2183019eac1eb98adaac781348619097
Tom Shaw <tom@tomshaw.info>
2013-07-20 23:54:58 -0500

    init

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+## Quick Log File Parser Code
+
+A quick node app to parse some simple log file data and store the info in a MYSQL database.
---
+++ app.js
@@ -0,0 +1,55 @@
+var fs = require('fs'),
+    mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    user: "root",
+    password: "password",
+    database: "project_belkinlog"
+});
+
+var formatDate = function (str) {
+    var parts = str.split("/");
+    return parts[2] + '-' + parts[0] + '-' + parts[1];
+}
+
+var selectData = function () {
+    connection.query('SELECT * FROM logs;', function (error, rows, fields) {
+        console.log(rows);
+    });
+}
+
+// Synchronous
+/*var array = fs.readFileSync('error.log').toString().split("\n");
+for (i in array) {
+    var row = array[i],
+        date = formatDate(row.substring(10, -10)),
+        time = row.slice(12, 20),
+        message = row.substring(21);
+    var insert = {
+        'logdate': date,
+        'logtime': time,
+        'message': message
+    };
+    connection.query('INSERT INTO logs SET ?', insert, function (err, result) {});
+}*/
+
+// Asynchronous:
+fs.readFile('error.log', function (err, data) {
+    if (err) throw err;
+    var split = data.toString().split("\n");
+    for (i in split) {
+        var row = split[i],
+            date = formatDate(row.substring(10, -10)),
+            time = row.slice(12, 20),
+            message = row.substring(21);
+        var array = {
+            'logdate': date,
+            'logtime': time,
+            'message': message
+        };
+        connection.query('INSERT INTO logs SET ?', array, function (err, result) {});
+    }
+    process.exit(code = 0);
+});
+
+console.log('BEGIN INSERT');
