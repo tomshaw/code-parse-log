50629856327ae39ea8e92289cffb880beae1386b
Tom Shaw <tom@tomshaw.info>
2013-07-21 16:13:54 -0500

    added callback

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ var formatDate = function (str) {
     return parts[2] + '-' + parts[0] + '-' + parts[1];
 }
 
-var selectData = function () {
+var selectData = function (cb) {
     connection.query('SELECT * FROM logs;', function (error, rows, fields) {
         console.log(rows);
+        return cb();
     });
 }
 
@@ -34,25 +35,34 @@ for (i in array) {
 }*/
 
 // Asynchronous:
-fs.readFile('error.log', function (err, data) {
-    if (err) throw err;
-    var split = data.toString().split("\n");
-    for (i in split) {
-        var row = split[i],
-            date = formatDate(row.substring(10, -10)),
-            time = row.slice(12, 20),
-            message = row.substring(21);
-        var array = {
-            'logdate': date,
-            'logtime': time,
-            'message': message
-        };
-        connection.query('INSERT INTO logs SET ?', array, function (err, result) {
-            if (err) throw err;
-        });
-    }
-    selectData();
-    //process.exit(code = 0);
-});
+var asynchronousStoreData = function (cb) {
+    console.log('BEGIN INSERT');
+    fs.readFile('error.log', function (err, data) {
+        if (err) throw err;
+        var split = data.toString().split("\n");
+        for (i in split) {
+            var row = split[i],
+                date = formatDate(row.substring(10, -10)),
+                time = row.slice(12, 20),
+                message = row.substring(21);
+            var array = {
+                'logdate': date,
+                'logtime': time,
+                'message': message
+            };
+            if (date && time && message) {
+                connection.query('INSERT INTO logs SET ?', array, function (err, result) {
+                    if (err) throw err;
+                });
+            }
+        }
+        return cb();
+    });
+}
 
-console.log('BEGIN INSERT');
+asynchronousStoreData(function () {
+    selectData(function () {
+        console.log('FINISHED INSERT');
+        process.exit(code = 0);
+    });
+})
